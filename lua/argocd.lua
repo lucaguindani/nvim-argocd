fd6d0082e730eba70b2cdb987598251c5e64e866
Luca Guindani <luca.guindani@gmail.com>
2025-06-02 00:45:15 +0200

    Update ArgoCD app list buffer to be non-modifiable but not readonly

--- lua/argocd.lua
+++ lua/argocd.lua
@@ -234,12 +234,12 @@ function M.list_apps()
   vim.cmd("enew")
   buf = vim.api.nvim_get_current_buf()
   vim.bo[buf].filetype = "argocd"
-  -- Make buffer non-modifiable and readonly to prevent insert mode
-  vim.bo[buf].modifiable = false
-  vim.bo[buf].readonly = true
+  -- Make buffer non-modifiable to prevent insert mode
   vim.bo[buf].buftype = "nofile"
   vim.bo[buf].bufhidden = "wipe"
   vim.bo[buf].swapfile = false
+  vim.bo[buf].modifiable = false
+  vim.bo[buf].readonly = false
 
   -- Disable orange highlight on line number for this buffer
   vim.api.nvim_buf_call(buf, function()
