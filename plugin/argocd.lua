51104d9a36d6df21e8892644a9ab86138d5a3d43
Luca Guindani <luca.guindani@gmail.com>
2025-06-03 16:35:11 +0200

    rename and rollback

--- plugin/argocd.lua
+++ plugin/argocd.lua
@@ -1,3 +1,6 @@
+-- plugin/argocd.lua
+-- This file is loaded automatically by Neovim when the plugin is installed
+
 -- Check Neovim version
 local v = vim.version()
 if not (v.major > 0 or (v.major == 0 and v.minor >= 7)) then
@@ -8,63 +11,38 @@ end
 -- Check if argocd is installed and load it
 local argocd_ok, argocd = pcall(require, "argocd")
 if not argocd_ok then
-  vim.notify("[argocd.nvim] failed to load plugin: " .. (argocd or "unknown error"), vim.log.levels.ERROR)
+  vim.notify("[argocd.nvim] failed to load plugin", vim.log.levels.ERROR)
   return
 end
 
 -- Check if plenary is installed
 local plenary_ok, _ = pcall(require, "plenary")
 if not plenary_ok then
-  vim.notify("[argocd.nvim] requires plenary", vim.log.levels.ERROR)
-  return
+  vim.notify("[argocd.nvim] requires plenary", vim.log.levels.WARN)
 end
 
 -- Set up the plugin with default commands
+
 vim.api.nvim_create_user_command("ArgoList", function()
-  if not argocd.is_logged_in() then
-    vim.notify("[argocd.nvim] Not logged in. Please login first.", vim.log.levels.ERROR)
-    return
-  end
-  argocd.list_apps()
+  argocd.lazy_login(argocd.list_apps)
 end, {})
 
 vim.api.nvim_create_user_command("ArgoSync", function(opts)
-  if not argocd.is_logged_in() then
-    vim.notify("[argocd.nvim] Not logged in. Please login first.", vim.log.levels.ERROR)
-    return
-  end
-  argocd.sync_app(opts.args)
+  argocd.lazy_login(function() argocd.sync_app(opts.args) end)
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("ArgoDelete", function(opts)
-  if not argocd.is_logged_in() then
-    vim.notify("[argocd.nvim] Not logged in. Please login first.", vim.log.levels.ERROR)
-    return
-  end
-  argocd.delete_app(opts.args)
+  argocd.lazy_login(function() argocd.delete_app(opts.args) end)
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("ArgoUpdate", function(opts)
-  if not argocd.is_logged_in() then
-    vim.notify("[argocd.nvim] Not logged in. Please login first.", vim.log.levels.ERROR)
-    return
-  end
-  argocd.update_app(opts.args)
+  argocd.lazy_login(function() argocd.update_app(opts.args) end)
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("ArgoPick", function()
-  if not argocd.is_logged_in() then
-    vim.notify("[argocd.nvim] Not logged in. Please login first.", vim.log.levels.ERROR)
-    return
-  end
-  argocd.telescope_apps()
+  argocd.lazy_login(argocd.telescope_apps)
 end, {})
 
 vim.api.nvim_create_user_command("ArgoLogout", function()
   argocd.clear_credentials()
-end, {})
-
--- Add login command
-vim.api.nvim_create_user_command("ArgoLogin", function()
-  argocd.login()
 end, {})
