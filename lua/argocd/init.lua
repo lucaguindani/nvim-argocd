1e3f9a468f8af349a63741bf3ea21d9a2c693026
Luca Guindani <luca.guindani@gmail.com>
2025-06-03 15:07:00 +0200

    Fix circular dependency issue

--- lua/argocd/init.lua
+++ lua/argocd/init.lua
@@ -1,22 +1,42 @@
 local M = {}
 
--- Load auth module and initialize
-local auth = require("argocd.auth")
-auth.load_credentials()
+-- Lazy load auth module
+function M.lazy_login()
+  return require("argocd.auth").lazy_login()
+end
 
--- Export public functions from auth module
-M.lazy_login = auth.lazy_login
-M.clear_credentials = auth.clear_credentials
-M.is_logged_in = auth.is_logged_in
+function M.clear_credentials()
+  return require("argocd.auth").clear_credentials()
+end
 
--- Export public functions from api module
-M.api_request = require("argocd.api").api_request
-M.sync_app = require("argocd.api").sync_app
-M.delete_app = require("argocd.api").delete_app
+function M.is_logged_in()
+  return require("argocd.auth").is_logged_in()
+end
 
--- Export public functions from ui module
-M.list_apps = require("argocd.ui").list_apps
-M.telescope_apps = require("argocd.ui").telescope_apps
-M.update_app = require("argocd.ui").update_app
+-- Lazy load API functions
+function M.api_request(...)
+  return require("argocd.api").api_request(...)
+end
+
+function M.sync_app(...)
+  return require("argocd.api").sync_app(...)
+end
+
+function M.delete_app(...)
+  return require("argocd.api").delete_app(...)
+end
+
+-- Lazy load UI functions
+function M.list_apps()
+  return require("argocd.ui").list_apps()
+end
+
+function M.telescope_apps()
+  return require("argocd.ui").telescope_apps()
+end
+
+function M.update_app(...)
+  return require("argocd.ui").update_app(...)
+end
 
 return M
